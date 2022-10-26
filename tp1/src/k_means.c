9630b96cecc7a6dbdd2d885db6f6fd9b34867e8b
jjgonc <josejoao_2001@hotmail.com>
2022-10-26 17:42:13 +0100

    new code and comments

--- tp1/src/k_means.c
+++ tp1/src/k_means.c
@@ -7,152 +7,161 @@
 #define N 10000000
 #define K 4
 
+/**
+ * @brief Struct to save a point that is part of the cluster
+ * 
+ */
+struct cluster_point{   
+    float x;
+    float y;
+    int cluster;
+};
 
-struct point{
+/**
+ * @brief Struct that holds the coordinates of a 2D point
+ * 
+ */
+struct point{   
     float x;
     float y;
 };
 
-struct cluster{
-    int size;
-    float center[2];
-    struct point *elements;
-};
 
 
-struct point *A;
-struct cluster *clusters, *old_clusters;
-
-void allocate()
-{
-    A = (struct point *)malloc(N*sizeof(struct point));
-    clusters = (struct cluster *)malloc(K*sizeof(struct cluster));
-    old_clusters = (struct cluster *)malloc(K*sizeof(struct cluster));
-    for(int i=0;i<K;i++)
-    {
-        clusters[i].size = 0;
-    }
-}
-
-void inicializa() {
+/**
+ * @brief Initializes cluster_point struct and initializes with a random value for its center
+ * 
+ * @param center The center of the cluster
+ * @param cluster_size The size of the cluster
+ * @return struct cluster_point* Returns a pointer to the new cluster_point struct
+ */
+struct cluster_point* inicializa(struct point *center, int *cluster_size) {
+    struct cluster_point *A = (struct cluster_point *)malloc(N*sizeof(struct cluster_point));
     srand(10);
-    for(int i = 0; i < N; i++) {
+    for(int i = 0; i < N; i++) {        //assigns random values to the coordinates
         A[i].x = (float) rand() / RAND_MAX;
         A[i].y = (float) rand() / RAND_MAX;
     }
-    for(int i = 0; i < K; i++) {
-        clusters[i].center[0] = A[i].x;
-        clusters[i].center[1] = A[i].y;
+    for(int i = 0; i < K; i++) {    // assign cluster number and its size
+        A[i].cluster = i;
+        center[i].x = A[i].x;
+        center[i].y = A[i].y;
+        cluster_size[i] = 0;
+    }
+    return A;
+}
+
+
+/**
+ * @brief Reevaluate centroides. Calculates the mean of each cluster and assigns that value to the cluster center
+ * 
+ * @param A The cluster_point struct
+ * @param center The center of the cluster
+ * @param size The size of the cluster
+ */
+void reevaluate_centers(struct cluster_point *A, struct point *center, int *size) {
+    // initialize variables
+    float sum_x[K], sum_y[K];
+    for(int i = 0; i < K; i++){
+        sum_x[i] = 0;
+        sum_y[i] = 0;
+        size[i] = 0;
+    }
+    // sum of coordinates in each cluster
+    for(int i = 0; i < N; i++){
+        sum_x[A[i].cluster] += A[i].x;
+        sum_y[A[i].cluster] += A[i].y;
+        size[A[i].cluster]++;
+    }
+
+    //calculates the result of the mean of the points of each cluster and assigns that value to the cluster center
+    for(int i = 0; i < K; i++){
+        center[i].x = sum_x[i] / size[i];
+        center[i].y = sum_y[i] / size[i];
     }
 }
 
-void cluster_points() {
+
+/**
+ * @brief Calculates new points and checks if the cluster center has changed
+ * 
+ * @param A The cluster_point struct
+ * @param center The center of the cluster
+ * @param size The size of the cluster
+ * @return int  1 if the cluster center has changed, 0 otherwise
+ */
+int cluster_points(struct cluster_point *A, struct point *center, int *size) {
+    int changed = 0;
     for(int i = 0; i < N; i++) {
-        float min_dist = 1000000000.0;
-        int index = 0;
+        float min_dist = 1000000000;
+        int min_cluster = 0;
+        //calculates the distance to the center of each cluster and saves the cluster with the smallest distance
         for(int j = 0; j < K; j++) {
-            float dist = sqrt(pow(A[i].x - clusters[j].center[0], 2) + pow(A[i].y - clusters[j].center[1], 2));
+            float dist = ((A[i].x - center[j].x)*(A[i].x - center[j].x)) + ((A[i].y - center[j].y)*(A[i].y - center[j].y));
             if(dist < min_dist) {
                 min_dist = dist;
-                index = j;
+                min_cluster = j;
             }
         }
-        clusters[index].elements = realloc(clusters[index].elements, (clusters[index].size + 1) * sizeof(struct point));
-        clusters[index].elements[clusters[index].size].x = A[i].x;
-        clusters[index].elements[clusters[index].size].y = A[i].y;
-        clusters[index].size++;
-    }
-}
-
-void clear_clusters(){
-    for(int i = 0; i < K; i++){
-        if(clusters[i].size > 0){
-            clusters[i].size = 0;
+        //Verify if the cluster has changed
+        if(A[i].cluster != min_cluster) {
+            A[i].cluster = min_cluster;
+            changed = 1;
         }
     }
-}
-
-void reevaluate_centers() {
-    for(int i = 0; i < K; i++) {
-        float sum_x = 0.0;
-        float sum_y = 0.0;
-        for(int j = 0; j < clusters[i].size; j++) {
-            sum_x += clusters[i].elements[j].x;
-            sum_y += clusters[i].elements[j].y;
-        }
-        clusters[i].center[0] = sum_x / clusters[i].size;
-        clusters[i].center[1] = sum_y / clusters[i].size;
-    }
-}
-
-int has_converged() {
-    for(int i = 0; i < K; i++) {
-        for(int j = 0; j < clusters[i].size; j++) {
-            if(clusters[i].elements[j].x != old_clusters[i].elements[j].x || 
-               clusters[i].elements[j].y != old_clusters[i].elements[j].y) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-void clone_cluster(){
-    for(int i = 0; i < K; i++) {
-        old_clusters[i].elements = realloc(old_clusters[i].elements, clusters[i].size * sizeof(struct point));
-        for(int j = 0; j < clusters[i].size; j++) {
-            old_clusters[i].elements[j].x = clusters[i].elements[j].x;
-            old_clusters[i].elements[j].y = clusters[i].elements[j].y;
-        }
-    }
+    //reevaluate the centers
+    reevaluate_centers(A, center, size);
+    return changed;
 }
 
 
-void print_clusters(int iter) {
+/**
+ * @brief Prints the cluster centers
+ * 
+ * @param iter The iteration number
+ * @param center The center of the cluster
+ * @param size The size of the cluster
+ */
+void print_clusters(int iter, struct point *center, int *size) {
     for(int i = 0; i < K; i++) {
-        printf("Center: (%.3f, %.3f) ", clusters[i].center[0], clusters[i].center[1]);
-        printf("Size : %d \n", clusters[i].size);
+        printf("Center: (%.3f, %.3f) ", center[i].x, center[i].y);
+        printf("Size : %d \n", size[i]);
     }
     printf("Iterations: %d \n", iter);
 }
 
-void free_clusters(){
-    free(A);
-    for(int i = 0; i < K; i++) {
-        free(clusters[i].elements);
-    }
-    free(clusters);
-    //for (int i = 0; i < K; i++) {
-    //    free(old_clusters[i].elements);
-    //} 
-    //free(old_clusters);
-}
 
+/**
+ * @brief Function that joins all the steps of the k-means algorithm
+ * 
+ */
+void k_means() {
+    //Initialize clusters
+    int size[K];
+    struct point center[K];
+    struct cluster_point* A = inicializa(center, size);
 
-int main()
-{
+    // start iteration counter
     int iter = 0;
-    //Iniciar clusters
-    allocate();
-    inicializa();
-    cluster_points();
-    reevaluate_centers();
-    clone_cluster();
-    clear_clusters();
-    //Primeira iteração
-    cluster_points();
-    reevaluate_centers();
-    iter++;
-    //Iterar até convergir
-    while (has_converged() == 0) {
-        clone_cluster();
-        clear_clusters();
-        cluster_points();
-        reevaluate_centers();
+    
+    // while the cluster center has changed
+    while(cluster_points(A, center, size) == 1) {
         iter++;
     }
-    print_clusters(iter);
-    free_clusters();
     
+    // print results whenever the cluster has not changed
+    print_clusters(iter, center, size);
+    
+    // release memory
+    free(A);
+}
+
+/**
+ * @brief Main function
+ * 
+ * @return int Returns 0 if the program runs successfully
+ */
+int main(){
+    k_means();
     return 0;
 }
